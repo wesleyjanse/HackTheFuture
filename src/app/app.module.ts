0a5f04a82fcd9a750ff11e5b43a5b18a133c5cec
Jelle VanLangendonck <r0705177@student.thomasmore.begit config --global user.email r0705177@student.thomasmore.be>
2019-11-23 16:20:45 +0100

    navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormSharedModule } from './core/form.module';
 import { MaterialModule } from './core/material.module';
-<<<<<<< HEAD
-import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-
-=======
 import { HomeComponent } from './components/home/home.component';
 import { AnomalyComponent } from './components/anomaly/anomaly.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterModule, Routes } from '@angular/router';
+import { MatToolbarModule, MatButtonModule, MatIconModule } from '@angular/material';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'anomaly', component: AnomalyComponent }
   ];
 
->>>>>>> routing added
 @NgModule({
   declarations: [
     AppComponent,
@@ -34,10 +30,15 @@ const appRoutes: Routes = [
     AppRoutingModule,
     MaterialModule,
     FormSharedModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   exports: [
     MaterialModule,
     FormSharedModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
