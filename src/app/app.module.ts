9cdbd9e52158915ab60d5538d361829dbdb7bc52
wesleyjanse <wesleyjanse@gmail.com>
2019-11-23 15:25:14 +0100

    bootstrap  & material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,42 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
 import { HomeComponent } from './home/home.component';
 import { AnomalyComponent } from './anomaly/anomaly.component';
 import { NavbarComponent } from './navbar/navbar.component';
+=======
+import { MaterialModule } from './core/material.module';
+import { FormSharedModule } from './core/form.module';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+>>>>>>> bootstrap  & material
 
+
+const appRoutes: Routes = [
+  { path: '', component: AppComponent },
+  ];
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< HEAD
     HomeComponent,
     AnomalyComponent,
     NavbarComponent
+=======
+    HomeComponent
+>>>>>>> bootstrap  & material
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    FormSharedModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
+  ],
+  exports: [
+    MaterialModule,
+    FormSharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
