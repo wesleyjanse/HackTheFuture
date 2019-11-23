fe47f1000abc00ef48e31b5d09f3383c79036d75
wesleyjanse <wesleyjanse@gmail.com>
2019-11-23 16:28:03 +0100

    Homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { fadeAnimation } from '../assets/fade.animation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [fadeAnimation]
 })
 export class AppComponent {
   title = 'hackthefuture';
+
+  public getRouterOutletState(outlet) {
+    return outlet.isActivated ? outlet.activatedRoute : '';
+  }
 }
+
+
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild, ElementRef  } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('myVideo', { static: true }) myVideo: ElementRef;
+  constructor(private router: Router) { }
 
-  constructor() { }
+  clicked: boolean = false;
 
   ngOnInit() {
   }
 
+  playVideo() {
+    this.clicked = true;
+    this.myVideo.nativeElement.play();
+    setTimeout( () => { this.router.navigate(['dashboard']); }, 4500 );
+
+    
+  }
 }
