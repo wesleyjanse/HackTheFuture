8c3cee99de37cf1f6807829cf571e22853b7ecbd
wesleyjanse <wesleyjanse@gmail.com>
2019-11-28 15:07:01 +0100

    fix alle banken

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -3,11 +3,8 @@ import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Bank } from '../models/bank.model';
 import { Transaction } from '../models/transaction.model';
-<<<<<<< HEAD
-=======
 import { BankAccount } from '../models/bankaccount.model';
-import { Account } from '../models/accounts.model';
->>>>>>> fix alle banken
+import { Account } from '../models/account.model';
 import { Person } from '../models/person.model';
 
 @Injectable({
